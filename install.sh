c4931d7e381ad612cd5f3c3df3aff0bdbc3bf4e5
长情坞 <105933272+Luv9-cn@users.noreply.github.com>
2025-11-11 23:28:47 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -80,7 +80,7 @@ INSTALL_DIR="/opt/socks5"
 SERVER_SCRIPT="socks5_light.sh"
 SERVICE_NAME="socks5-proxy"
 PORT=1080  # 默认使用1080端口
-GITHUB_REPO="https://raw.githubusercontent.com/username/socks5/master"
+GITHUB_REPO="https://raw.githubusercontent.com/Luv9-cn/socks5/master"
 
 # 获取服务器IP
 SERVER_IP=$(get_server_ip)
